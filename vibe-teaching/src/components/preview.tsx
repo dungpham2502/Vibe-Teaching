7e33ea9c788c511f028c891f7ec705cba5e850b5
minhtq05 <minhtq2005@gmail.com>
2025-04-05 16:38:41 -0400

    remotion preview

---
+++ vibe-teaching/src/components/preview.tsx
@@ -0,0 +1,55 @@
+import { Player } from "@remotion/player";
+import { RemotionObject, Scene } from "@/types/remotion-types";
+import { AbsoluteFill, Series } from "remotion";
+
+export function Preview({ scenes }: { scenes: Scene[] }) {
+	return (
+		<div className="bg-black w-[1000px]">
+			<div className="w-full aspect-video flex flex-col justify-center items-center border-red border-[1px] rounded bg-gray">
+				<Player
+					fps={30}
+					component={VideoComponent}
+					inputProps={{ scenes }}
+					durationInFrames={240}
+					compositionHeight={1080}
+					compositionWidth={1920}
+					loop
+					style={{
+						width: "100%",
+					}}
+					controls
+					acknowledgeRemotionLicense
+				/>
+			</div>
+		</div>
+	);
+}
+
+// TODO: Handle Video, Image, Audio;
+
+function VideoComponent({ scenes }: { scenes: Scene[] }) {
+	return (
+		<Series>
+			{scenes.map((scene: Scene) => (
+				<Series.Sequence durationInFrames={scene.durationInFrames}>
+					<AbsoluteFill>
+						{scene.children.map((item: RemotionObject) => {
+							switch (item.type) {
+								case "title":
+									return <p className="text-9xl">{item.text}</p>;
+								case "subtitle":
+									return <p className="text-2xl">{item.text}</p>;
+								case "heading":
+									return <p className="text-7xl">{item.text}</p>;
+								case "paragraph":
+									return <p className="text-2xl">{item.text}</p>;
+								default:
+									return <div>Item not supported</div>;
+							}
+						})}
+					</AbsoluteFill>
+				</Series.Sequence>
+			))}
+		</Series>
+	);
+}
